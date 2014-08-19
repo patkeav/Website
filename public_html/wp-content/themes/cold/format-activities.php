7e8c39b2f5d744fdb2441b0ab5a95914ea8aed1f
Patrick Keaveny <keavenyp@gmail.com>
2014-08-19 16:39:47 -0600

    Moved directories
    Moved images and css to a new directory.

---
+++ public_html/wp-content/themes/cold/format-activities.php
@@ -0,0 +1,18 @@
+
+<h1> Activities </h1>
+
+<div id="activities" class="entryRes" >
+
+	<button id="show" class="showHide" >Click to Show</button><button id="hide" class="showHide hide" >Click to Hide</button>
+	<p>&nbsp;</p>
+
+	<span class="deetsContent">
+		<ul >
+			<li>Creighton Swing Dance Club - Fall 2009 - Fall 2010</li>
+			<li>Creighton Ski/Snowboard Club - Fall 2009</li>
+			<li>Creighton FaceAids - Spring 2012 to present </li>
+		</ul>
+		
+	</span>
+
+</div>
